fa12e6a5ce9cf3bedb9d526e61648c8b57e573a3
1995F150 <jessiecrider3@gmail.com>
2025-07-03 15:29:28 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-    apiKey: "your-firebase-api-key",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id",
-    measurementId: "your-measurement-id"
+    apiKey: "AIzaSyAIWOdp6m3JMOLeQ-rIgIfJ-8VwkwBpJ5k",
+    authDomain: "crideros-sign-in.firebaseapp.com",
+    projectId: "crideros-sign-in",
+    storageBucket: "crideros-sign-in.appspot.com",
+    messagingSenderId: "470579826527",
+    appId: "1:470579826527:web:09273c1f6693676120b0ea",
+    measurementId: "G-XT07NH6NBF
+"
 };
